ae63e9c9285c08dc8893d6fa43512279abd022e7
MrPingviin <foldimogyoro2000@gmail.com>
2023-01-01 14:46:28 +0100

    Hotfix - Dummy redirects are removed

--- src/pages/_Profile.jsx
+++ src/pages/_Profile.jsx
@@ -17,15 +17,15 @@ console.log("SOROS", userDataContainer)
 return (
     <MainContent>
     <ProfileCard>
-        <ProfileAvatar src={userDataContainer[0].images[0].url} onClick={() => window.location.href = "https://open.spotify.com/user/mrpingviin"} width="50%" className="Rounded Clickable"/>
+        <ProfileAvatar src={userDataContainer[0].images[0].url} onClick={() => window.location.href = userDataContainer[0].href} width="50%" className="Rounded Clickable"/>
         <ProfileName onClick={() => window.location.href = userDataContainer[0].href}>{userDataContainer[0].display_name}</ProfileName>
         <ProfileType type={userDataContainer[0].product}/>
         <ProfileStats/>
         <Button onClick={() => (setActualPage("Login")) (logout())} className="Outlined">Logout</Button>
     </ProfileCard>
     <Splitter>
-      <Toplist title="Top Artists of All Time" onClick={() => alert("WIP")} type="Artist" short="true"></Toplist>
-      <Toplist title="Top Tracks of All Time" onClick={() => alert("WIP")} type="Song" short="true"></Toplist>
+      <Toplist title="Top Artists of All Time" onClick={() => setActualCategory("Top_Artists")} type="Artist" short="true"></Toplist>
+      <Toplist title="Top Tracks of All Time" onClick={() => setActualCategory("Top_Tracks")} type="Song" short="true"></Toplist>
     </Splitter>
 </MainContent>
 )
